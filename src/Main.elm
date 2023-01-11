b70112e55cf25da3e2efae322130e30134a4e084
Gabriela Sartori <gabriel.sartori04@gmail.com>
2023-01-10 23:29:59 -0300

    feat: settings panel

--- src/Main.elm
+++ src/Main.elm
@@ -16,7 +16,6 @@ import Set exposing (Set)
 
 -- TODO: options to include dakuon and combos
 -- TODO: katakana version
--- TODO: improve interface
 
 
 main : Program () Model Msg
@@ -31,38 +30,83 @@ main =
 
 type alias Model =
     { hiraganas : List Char
-    , input : String
+    , answer : String
     , error : Bool
     , corrects : Int
     , incorrects : Int
     , errors : Set Char
     , reviewing : Bool
-    , level : Int
+    , options : Options
+    }
+
+
+type alias Options =
+    { level : Int
+    , hiraganasVowels : Bool
+    , hiraganasK : Bool
+    , hiraganasS : Bool
+    , hiraganasT : Bool
+    , hiraganasN : Bool
+    , hiraganasH : Bool
+    , hiraganasM : Bool
+    , hiraganasY : Bool
+    , hiraganasR : Bool
+    , hiraganasW : Bool
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { hiraganas = [ 'あ' ]
-      , input = ""
+      , answer = ""
       , error = False
       , corrects = 0
       , incorrects = 0
       , errors = Set.empty
       , reviewing = False
-      , level = 1
+      , options = initOptions
       }
-    , generateHiraganas 1
+    , generateHiraganas initOptions
     )
+
+
+initOptions : Options
+initOptions =
+    { level = 1
+    , hiraganasVowels = True
+    , hiraganasK = False
+    , hiraganasS = False
+    , hiraganasT = False
+    , hiraganasN = False
+    , hiraganasH = False
+    , hiraganasM = False
+    , hiraganasY = False
+    , hiraganasR = False
+    , hiraganasW = False
+    }
 
 
 type Msg
     = GotHiraganas (List Char)
-    | OnInput String
+    | OnInput InputType
     | Submit
     | ChangeLevel
     | Review
     | Reset
+
+
+type InputType
+    = Answer String
+    | HiraganasVowels Bool
+    | HiraganasK Bool
+    | HiraganasS Bool
+    | HiraganasT Bool
+    | HiraganasN Bool
+    | HiraganasH Bool
+    | HiraganasM Bool
+    | HiraganasY Bool
+    | HiraganasR Bool
+    | HiraganasW Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -71,17 +115,82 @@ update msg model =
         GotHiraganas hiraganas_ ->
             ( { model | hiraganas = hiraganas_ }, Cmd.none )
 
-        OnInput str ->
-            ( { model | input = str }, Cmd.none )
+        OnInput inputType ->
+            let
+                { options } =
+                    model
+
+                newModel =
+                    case inputType of
+                        Answer answer ->
+                            { model | answer = answer }
+
+                        HiraganasVowels value ->
+                            { model | options = { options | hiraganasVowels = value } }
+
+                        HiraganasK value ->
+                            { model | options = { options | hiraganasK = value } }
+
+                        HiraganasS value ->
+                            { model | options = { options | hiraganasS = value } }
+
+                        HiraganasT value ->
+                            { model | options = { options | hiraganasT = value } }
+
+                        HiraganasN value ->
+                            { model | options = { options | hiraganasN = value } }
+
+                        HiraganasH value ->
+                            { model | options = { options | hiraganasH = value } }
+
+                        HiraganasM value ->
+                            { model | options = { options | hiraganasM = value } }
+
+                        HiraganasY value ->
+                            { model | options = { options | hiraganasY = value } }
+
+                        HiraganasR value ->
+                            { model | options = { options | hiraganasR = value } }
+
+                        HiraganasW value ->
+                            { model | options = { options | hiraganasW = value } }
+
+                noCharacterSelected : Bool
+                noCharacterSelected =
+                    let
+                        o : Options
+                        o =
+                            newModel.options
+                    in
+                    [ o.hiraganasVowels
+                    , o.hiraganasK
+                    , o.hiraganasS
+                    , o.hiraganasT
+                    , o.hiraganasN
+                    , o.hiraganasH
+                    , o.hiraganasM
+                    , o.hiraganasY
+                    , o.hiraganasR
+                    , o.hiraganasW
+                    ]
+                        |> List.any identity
+            in
+            ( if noCharacterSelected then
+                newModel
+
+              else
+                model
+            , Cmd.none
+            )
 
         Submit ->
-            if matches model.hiraganas model.input then
-                ( { model | error = False, input = "", corrects = model.corrects + 1 }
+            if matches model.hiraganas model.answer then
+                ( { model | error = False, answer = "", corrects = model.corrects + 1 }
                 , if model.reviewing then
                     generateReviewHiragana model.errors
 
                   else
-                    generateHiraganas model.level
+                    generateHiraganas model.options
                 )
 
             else
@@ -95,15 +204,21 @@ update msg model =
 
         ChangeLevel ->
             let
-                ( model_, _ ) =
+                ( newModel, _ ) =
                     init
+
+                { options } =
+                    model
 
                 level : Int
                 level =
-                    Basics.modBy 10 model.level + 1
+                    Basics.modBy 10 options.level + 1
+
+                newOptions =
+                    { options | level = level }
             in
-            ( { model_ | level = level }
-            , generateHiraganas level
+            ( { newModel | options = newOptions }
+            , generateHiraganas newOptions
             )
 
         Review ->
@@ -112,15 +227,26 @@ update msg model =
             )
 
         Reset ->
-            init
+            let
+                ( newModel, _ ) =
+                    init
+            in
+            ( { newModel | options = model.options }
+            , generateHiraganas model.options
+            )
 
 
-generateHiraganas : Int -> Cmd Msg
-generateHiraganas size =
+generateHiraganas : Options -> Cmd Msg
+generateHiraganas options =
     let
+        availableHiraganas =
+            options
+                |> optionsToHiraganas
+                |> List.map Tuple.first
+
         generator : Random.Generator Char
         generator =
-            case List.map Tuple.first hiraganas of
+            case availableHiraganas of
                 h :: tail ->
                     Random.uniform h tail
 
@@ -128,7 +254,7 @@ generateHiraganas size =
                     Random.constant '?'
     in
     generator
-        |> Random.list size
+        |> Random.list options.level
         |> Random.generate GotHiraganas
 
 
@@ -155,11 +281,11 @@ matches hiragana input =
 
 
 hiraganaToRomaji : List Char -> String
-hiraganaToRomaji hiragana =
-    hiragana
+hiraganaToRomaji hiraganas =
+    hiraganas
         |> List.map
             (\hiragana_ ->
-                hiraganas
+                allHiraganas
                     |> Dict.fromList
                     |> Dict.get hiragana_
                     |> Maybe.withDefault "?"
@@ -167,26 +293,167 @@ hiraganaToRomaji hiragana =
         |> String.join ""
 
 
-hiraganas : List ( Char, String )
-hiraganas =
-    [ [ ( 'あ', "a" ), ( 'い', "i" ), ( 'う', "u" ), ( 'え', "e" ), ( 'お', "o" ) ]
-    , [ ( 'か', "ka" ), ( 'き', "ki" ), ( 'く', "ku" ), ( 'け', "ke" ), ( 'こ', "ko" ) ]
-    , [ ( 'さ', "sa" ), ( 'し', "shi" ), ( 'す', "su" ), ( 'せ', "se" ), ( 'そ', "so" ) ]
-    , [ ( 'た', "ta" ), ( 'ち', "chi" ), ( 'つ', "tsu" ), ( 'て', "te" ), ( 'と', "to" ) ]
-    , [ ( 'な', "na" ), ( 'に', "ni" ), ( 'ぬ', "nu" ), ( 'ね', "ne" ), ( 'の', "no" ) ]
-    , [ ( 'は', "ha" ), ( 'ひ', "hi" ), ( 'ふ', "fu" ), ( 'へ', "he" ), ( 'ほ', "ho" ) ]
-    , [ ( 'ま', "ma" ), ( 'み', "mi" ), ( 'む', "mu" ), ( 'め', "me" ), ( 'も', "mo" ) ]
-    , [ ( 'や', "ya" ), ( 'ゆ', "yu" ), ( 'よ', "yo" ) ]
-    , [ ( 'ら', "ra" ), ( 'り', "ri" ), ( 'る', "ru" ), ( 'れ', "re" ), ( 'ろ', "ro" ) ]
-    , [ ( 'わ', "wa" ), ( 'を', "wo" ) ]
+allHiraganas : List ( Char, String )
+allHiraganas =
+    [ hiraganasVowels
+    , hiraganasK
+    , hiraganasS
+    , hiraganasT
+    , hiraganasN
+    , hiraganasH
+    , hiraganasM
+    , hiraganasY
+    , hiraganasR
+    , hiraganasW
     ]
         |> List.concat
 
 
+optionsToHiraganas : Options -> List ( Char, String )
+optionsToHiraganas options =
+    [ if options.hiraganasVowels then
+        hiraganasVowels
+
+      else
+        []
+    , if options.hiraganasK then
+        hiraganasK
+
+      else
+        []
+    , if options.hiraganasS then
+        hiraganasS
+
+      else
+        []
+    , if options.hiraganasT then
+        hiraganasT
+
+      else
+        []
+    , if options.hiraganasN then
+        hiraganasN
+
+      else
+        []
+    , if options.hiraganasH then
+        hiraganasH
+
+      else
+        []
+    , if options.hiraganasM then
+        hiraganasM
+
+      else
+        []
+    , if options.hiraganasY then
+        hiraganasY
+
+      else
+        []
+    , if options.hiraganasR then
+        hiraganasR
+
+      else
+        []
+    , if options.hiraganasW then
+        hiraganasW
+
+      else
+        []
+    ]
+        |> List.concat
+
+
+hiraganasVowels =
+    [ ( 'あ', "a" )
+    , ( 'い', "i" )
+    , ( 'う', "u" )
+    , ( 'え', "e" )
+    , ( 'お', "o" )
+    ]
+
+
+hiraganasK =
+    [ ( 'か', "ka" )
+    , ( 'き', "ki" )
+    , ( 'く', "ku" )
+    , ( 'け', "ke" )
+    , ( 'こ', "ko" )
+    ]
+
+
+hiraganasS =
+    [ ( 'さ', "sa" )
+    , ( 'し', "shi" )
+    , ( 'す', "su" )
+    , ( 'せ', "se" )
+    , ( 'そ', "so" )
+    ]
+
+
+hiraganasT =
+    [ ( 'た', "ta" )
+    , ( 'ち', "chi" )
+    , ( 'つ', "tsu" )
+    , ( 'て', "te" )
+    , ( 'と', "to" )
+    ]
+
+
+hiraganasN =
+    [ ( 'な', "na" )
+    , ( 'に', "ni" )
+    , ( 'ぬ', "nu" )
+    , ( 'ね', "ne" )
+    , ( 'の', "no" )
+    ]
+
+
+hiraganasH =
+    [ ( 'は', "ha" )
+    , ( 'ひ', "hi" )
+    , ( 'ふ', "fu" )
+    , ( 'へ', "he" )
+    , ( 'ほ', "ho" )
+    ]
+
+
+hiraganasM =
+    [ ( 'ま', "ma" )
+    , ( 'み', "mi" )
+    , ( 'む', "mu" )
+    , ( 'め', "me" )
+    , ( 'も', "mo" )
+    ]
+
+
+hiraganasY =
+    [ ( 'や', "ya" )
+    , ( 'ゆ', "yu" )
+    , ( 'よ', "yo" )
+    ]
+
+
+hiraganasR =
+    [ ( 'ら', "ra" )
+    , ( 'り', "ri" )
+    , ( 'る', "ru" )
+    , ( 'れ', "re" )
+    , ( 'ろ', "ro" )
+    ]
+
+
+hiraganasW =
+    [ ( 'わ', "wa" )
+    , ( 'を', "wo" )
+    ]
+
+
 view : Model -> E.Element Msg
 view model =
-    E.el [ E.paddingXY 64 32, E.centerX, EBO.rounded 8, EBO.width 1 ] <|
-        E.column [ E.centerX ]
+    E.row [ E.centerX, E.spacing 8 ]
+        [ E.column [ E.height E.fill, E.paddingXY 64 32, E.centerX, EBO.rounded 8, EBO.width 1 ]
             [ E.paragraph [ EF.size 64 ]
                 [ E.text (String.fromList model.hiraganas)
                 ]
@@ -198,8 +465,8 @@ view model =
                     E.none
                 ]
             , EI.text [ EF.size 24, onEnter Submit ]
-                { onChange = OnInput
-                , text = model.input
+                { onChange = OnInput << Answer
+                , text = model.answer
                 , placeholder = Nothing
                 , label = EI.labelHidden "input"
                 }
@@ -216,7 +483,7 @@ view model =
                         )
                     ]
             , E.el [ E.centerX, E.paddingXY 0 4 ] <|
-                viewButton ChangeLevel ("Level " ++ String.fromInt model.level)
+                viewButton ChangeLevel ("Level " ++ String.fromInt model.options.level)
             , let
                 errors : Int
                 errors =
@@ -230,7 +497,7 @@ view model =
                     else
                         "s"
               in
-              if model.level > 1 || model.reviewing || errors == 0 then
+              if model.options.level > 1 || model.reviewing || errors == 0 then
                 E.none
 
               else
@@ -243,6 +510,60 @@ view model =
                 E.el [ E.centerX, E.paddingXY 0 4 ] <|
                     viewButton Reset "Reset"
             ]
+        , E.column [ E.height E.fill, E.paddingXY 64 32, E.spacing 12, E.centerX, EBO.rounded 8, EBO.width 1 ]
+            [ E.paragraph [] [ E.text "Settings" ]
+            , viewCheckbox
+                { onChange = OnInput << HiraganasVowels
+                , label = "Vowels"
+                , checked = model.options.hiraganasVowels
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasK
+                , label = "K-"
+                , checked = model.options.hiraganasK
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasS
+                , label = "S-"
+                , checked = model.options.hiraganasS
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasT
+                , label = "T-"
+                , checked = model.options.hiraganasT
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasN
+                , label = "N-"
+                , checked = model.options.hiraganasN
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasH
+                , label = "H-"
+                , checked = model.options.hiraganasH
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasM
+                , label = "M-"
+                , checked = model.options.hiraganasM
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasY
+                , label = "Y-"
+                , checked = model.options.hiraganasY
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasR
+                , label = "R-"
+                , checked = model.options.hiraganasR
+                }
+            , viewCheckbox
+                { onChange = OnInput << HiraganasW
+                , label = "W-"
+                , checked = model.options.hiraganasW
+                }
+            ]
+        ]
 
 
 viewButton : Msg -> String -> E.Element Msg
@@ -250,6 +571,21 @@ viewButton msg label =
     EI.button [ E.centerX, E.width (E.px 200), E.height (E.px 32), EBO.rounded 4, EBO.width 1 ]
         { onPress = Just msg
         , label = E.text label
+        }
+
+
+viewCheckbox :
+    { onChange : Bool -> Msg
+    , label : String
+    , checked : Bool
+    }
+    -> E.Element Msg
+viewCheckbox { label, onChange, checked } =
+    EI.checkbox []
+        { onChange = onChange
+        , icon = EI.defaultCheckbox
+        , checked = checked
+        , label = EI.labelRight [] (E.text label)
         }
 
 
@@ -268,59 +604,3 @@ onEnter msg =
                     )
             )
         )
-
-
-
--- H.div
---     []
---     [ H.p
---         [ HA.style "font-size" "64px"
---         , HA.style "line-height" "0px"
---         , HA.style "font-family" "\"Lucida Console\", \"Courier New\", monospace"
---         ]
---         [ H.text (String.fromList model.hiraganas) ]
---     , if model.error then
---         H.text ("Correct: " ++ hiraganaToRomaji model.hiraganas)
---       else
---         E.none
---     , H.form [ HE.onSubmit Submit ]
---         [ H.input [ HE.onInput OnInput, HA.value model.input ] []
---         ]
---     , if model.reviewing then
---         H.p [] []
---       else
---         H.p []
---             [ H.text
---                 ("✅ "
---                     ++ String.fromInt model.corrects
---                     ++ " ❌ "
---                     ++ String.fromInt model.incorrects
---                 )
---             ]
---     , H.div
---         [ HA.style "display" "flex"
---         , HA.style "flex-direction" "column"
---         , HA.style "gap" "4px"
---         ]
---         [ H.button [ HE.onClick ChangeLevel ]
---             [ H.text ("Level " ++ String.fromInt model.level) ]
---         , let
---             errors =
---                 Set.size model.errors
---             plural =
---                 if errors == 1 then
---                     ""
---                 else
---                     "s"
---           in
---           if model.reviewing || errors == 0 then
---             E.none
---           else
---             H.button [ HE.onClick Review ]
---                 [ H.text ("Review " ++ String.fromInt errors ++ " error" ++ plural) ]
---         , if model == Tuple.first init then
---             E.none
---           else
---             H.button [ HE.onClick Reset ] [ H.text "Reset" ]
---         ]
---     ]
